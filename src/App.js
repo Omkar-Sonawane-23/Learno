2526728d94b4621f2b5cd73de74c6133bf11af9b
Omkar <sonawaneomkar2306@gmail.com>
2024-05-29 18:52:39 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import CameraComponent from './components/CameraComponent';
-import QuestionComponent from './components/QuestionComponent';
+import QuestionComponent from './components/QuestionComponent.js';
 import './App.css';
 
 const App = () => {
