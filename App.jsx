50a142f695ca48a881a14661c8fc0843ce967b31
Omkar Sonawane <130058150+Omkar-Sonawane-23@users.noreply.github.com>
2024-05-29 18:32:29 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+// src/App.js
+import React from 'react';
+import CameraComponent from './components/CameraComponent';
+import QuestionComponent from './components/QuestionComponent';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>English Speaking Practice</h1>
+      </header>
+      <main>
+        <CameraComponent />
+        <QuestionComponent />
+      </main>
+    </div>
+  );
+};
+
+export default App;
